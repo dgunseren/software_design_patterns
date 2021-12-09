396dfe6fcdbf96eb0f46cad5de05d6d155241d13
@deniz.gunseren <deniz.gunseren@ozu.edu.tr>
2021-12-09 21:18:30 +0300

    minor change in naming 4

--- src/CreditAccountVisitor.java
+++ src/CreditAccountVisitor.java
@@ -1,6 +1,4 @@
 public class CreditAccountVisitor implements Visitor{
-
-
     @Override
     public void visit(CreditAccount creditAccount) {
         int cash_balance = new CreditAccount().cashAccountBalance();
--- src/VisitableAccount.java
+++ src/VisitableAccount.java
@@ -1,7 +1,5 @@
 import java.util.List;
 
-
-
 public interface VisitableAccount {
     void accept(Visitor visitor);
 }

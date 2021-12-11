ac7c9302f11bfffde4ef8d7dea8734c4189f91de
Atakan Ataman Atik <atakanatamana@gmail.com>
2021-12-11 17:18:26 +0300

    database driver added and connection is successful.

--- src/runner.java
+++ src/runner.java
@@ -1,4 +1,9 @@
 import modals.MoneyReserve;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /*
     LET'S WRITE WHICH DESIGN PATTERN USED IN WHICH PART OF THE SYSTEM
@@ -13,5 +18,17 @@ class runner{
         System.out.println("total money in reserve " + MoneyReserve.MONEY_RESERVE.getTotalMoneyInReserve());
         MoneyReserve.MONEY_RESERVE.takeFromReserve(2000);
         System.out.println("total money in reserve " + MoneyReserve.MONEY_RESERVE.getTotalMoneyInReserve());
+
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/bankingapi", "postgres", "postgres")) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM people");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name") + " " + resultSet.getString("surname") + " " + resultSet.getString("address"));
+            }
+
+        }  catch (SQLException e) {
+            System.out.println("Connection failure.");
+            e.printStackTrace();
+        }
     }
 }

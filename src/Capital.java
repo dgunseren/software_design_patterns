34f536a741e0e999fbcf97a35b99203a5e8ea319
@deniz.gunseren <deniz.gunseren@ozu.edu.tr>
2022-01-03 00:22:01 +0300

    Observer is added

---
+++ src/Capital.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capital {
+
+    private List<Observer> observers = new ArrayList<Observer>();
+    private double amount;
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+        notifyAllObservers();
+    }
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void notifyAllObservers(){
+        for (Observer observer : observers) {
+            observer.update();
+        }
+    }
+}
---
+++ src/Observer.java
@@ -0,0 +1,4 @@
+public abstract class Observer {
+    protected Capital capital;
+    public abstract void update();
+}
